502ba35ef02f9ee2920d34e2e09fde092f78b7d3
Eduardo Jesús Díaz Dioniz <eddioniz@gmail.com>
2014-11-04 16:04:21 +0000

    Add father in node class

--- lib/nodelist/list.rb
+++ lib/nodelist/list.rb
@@ -1,31 +1,31 @@
 
 
 class Lista
-	attr_accessor :head, :lastnext
-	Node = Struct.new(:value, :next)
-	def initialize()
-		@head = nil
-		@lastnext = nil
-	end
+  attr_accessor :head, :lastnext
+  Node = Struct.new(:value, :next, :father)
+  def initialize()
+    @head = nil
+    @lastnext = nil
+  end
 
-	def add(*args)
-		args.each do |value|
-			if @head == nil	then
-				@head = Node.new(value, nil)
-				@lastnext = @head
-			else
-				last = Node.new(value, nil)
-				@lastnext.next = last
-				@lastnext = last
-			end
-		end
-	end
-	def first
-    	@head.value
-	end
-	def to_s
-		"#{@head}"
-	end
+  def add(*args)
+    args.each do |value|
+      if @head == nil then
+        @head = Node.new(value, nil, nil)
+        @lastnext = @head
+      else
+        last = Node.new(value, nil, @lastnext)
+        @lastnext.next = last
+        @lastnext = last
+      end
+    end
+  end
+  def first
+    @head.value
+  end
+  def to_s
+    "#{@head}"
+  end
   def to_a
     a_value = Array.new
     pointer = @head
@@ -38,20 +38,19 @@ class Lista
   end
 end
 if __FILE__ == $0 then
-	require '../question/simpleChoice/base'
+  require '../question/simpleChoice/base'
 
-	lista1 = Lista.new()
-	lista1.add(1,2,3)
-	lista1.add(4)
-	puts "Uso de la función first:"
-	puts lista1.first
-	puts "-- Estructura de la lista enlazada --"
-  	puts lista1
-  	puts "-- -- --"
-  	puts lista1.to_a
-
-
-	listQuestion = Lista.new()
+  lista1 = Lista.new()
+  lista1.add(1,2,3)
+  lista1.add(4)
+  puts "Uso de la función first:"
+  puts lista1.first
+  puts "-- Estructura de la lista enlazada --"
+  puts lista1
+  puts "-- -- --"
+  puts lista1.to_a
+  
+  listQuestion = Lista.new()
 
 	#Preguntas para el test de la practica 06
 	p1 = Question::SimpleChoice.new(:text =>"¿Cual es la salida del siguiente código Ruby ? \n class Xyz \n    def pots \n     @nice\n   end\n   end\n", :right => 'nil' ,:distractor => ['#<Xyz: 0xa000208>','0','Nunguna de las anteriores'] )
