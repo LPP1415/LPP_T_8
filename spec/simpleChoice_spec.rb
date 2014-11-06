87af9a9734a8c7b542f2e957afb369d7dea32468
Eduardo Jesús Díaz Dioniz <eddioniz@gmail.com>
2014-11-06 14:10:14 +0000

    fixed error spec

--- spec/simpleChoice_spec.rb
+++ spec/simpleChoice_spec.rb
@@ -1,14 +1,29 @@
 require './lib/question/simpleChoice'
+require './lib/question/trueOrFalse'
+
 module Question
+  class TrueOrFalse
+    describe Question::TrueOrFalse do
+      before :each do
+        @ptf = Question::TrueOrFalse.new({:text=>'2+2=4',:right=>'true'})
+      end
+      context "Question True/false" do
+        it "tiene los valores esperados"do
+          expect(@ptf.text) == '2 + 2 ='
+          expect(@ptf.right) == 'true'
+        end
+      end
+    end
+  end
   class SimpleChoice
-  describe  Question::SimpleChoice do   
-    before :each do
-      @sc = Question::SimpleChoice.new(:text => '2 + 2 =',
+    describe  Question::SimpleChoice do
+      before :each do
+        @sc = Question::SimpleChoice.new(:text => '2 + 2 =',
                                        :right => 4,
                                        :distractor => [9,3,1])
     #Preguntas para el test de la practica 06
-	@p1 = Question::SimpleChoice.new(:text =>"¿Cual es la salida del siguiente código Ruby ? \n class Xyz \n    def pots \n     @nice\n   end\n   end\n", :right => 'nil' ,:distractor => ['#<Xyz: 0xa000208>','0','Nunguna de las anteriores'] )
-	@p2 = Question::SimpleChoice.new(:text => "La siguiente definici´on de un hash en Ruby es v´alida:
+        @p1 = Question::SimpleChoice.new(:text =>"¿Cual es la salida del siguiente código Ruby ? \n class Xyz \n    def pots \n     @nice\n   end\n   end\n", :right => 'nil' ,:distractor => ['#<Xyz: 0xa000208>','0','Nunguna de las anteriores'] )
+        @p2 = Question::SimpleChoice.new(:text => "La siguiente definici´on de un hash en Ruby es v´alida:
 
 hash_raro = {
 
@@ -18,7 +33,7 @@ Hash.new => :toto
 
 }", :right => 'falso', :distractor => 'Cierto')
 
-	@p3 = Question::SimpleChoice.new(:text =>" ¿Cu´al es la salida del siguiente c´odigo Ruby?
+        @p3 = Question::SimpleChoice.new(:text =>" ¿Cu´al es la salida del siguiente c´odigo Ruby?
 
 class Array
 
@@ -31,55 +46,54 @@ end
 end
 
 p [1, \"bob\"].say_hi", :right =>'"HEY!"', :distractor => ['1','bob','Nunguna de las anteriores'] )
-	@p4 = Question::SimpleChoice.new(:text => "¿Cu´al es el tipo del objeto en el siguiente c´odigo Ruby?
+        @p4 = Question::SimpleChoice.new(:text => "¿Cu´al es el tipo del objeto en el siguiente c´odigo Ruby?
 
 class Objeto
 
 end
 ", :right =>'Un objeto', :distractor => ['Una instancia de una clase', 'Una constante', 'Ninguna de las anteriores'] )
-	@p5 = Question::SimpleChoice.new(:text => " Es apropiado que una clase Tablero herede de una clase Juego.
+        @p5 = Question::SimpleChoice.new(:text => " Es apropiado que una clase Tablero herede de una clase Juego.
 ", :right => 'Cierto', :distractor => 'Falso')
 
+
+      end
+      context "Creando una pregunta" do
+        it "Se crea correctamente" do
+          expect(@sc.text) == '2 + 2 ='
+          expect(@sc.right) == 4
+          expect(@sc.distractor) == [9,3,1]
+        end
+        it "Pregunta 1" do
+          expect(@p1.right) == 'nil'
+        end
+        it "Pregunta 2" do 
+          expect(@p2.right) == 'falso'
+        end
+        it "Pregunta 3"do
+          expect(@p3.right) == '"HEY!"'
+        end
+        it "Pregunta 4"do 
+          expect(@p4.right) == 'Un objeto'
+        end
+        it "Pregunta 5"do
+          expect(@p5.right) == 'Cierto'
+        end 
+
+        it "Has to have the three components" do
+          expect { Question::SimpleChoice.new(:text=>'5*8=?')}.to raise_error(ArgumentError)
+        end
+      end
+
+      context "Whene converting" do
+        it "can be converted to html" do
+          expect(@sc).to respond_to :to_html
+        end
+
+        it "has to produce a reasonable HTML" do
+          r= %r{<h3>(.*)<\/h3><br\/><input type=\"radio\" value=\"\d\" name=\"0\"\/>\d.*}
+          expect( @sc.to_html ).to match(r)
+        end
+      end
     end
-    context "Creando una pregunta" do
-      it "Se crea correctamente" do
-        expect(@sc.text) == '2 + 2 ='
-        expect(@sc.right) == 4
-        expect(@sc.distractor) == [9,3,1]
-      end
-      it "Pregunta 1" do
-	expect(@p1.right) == 'nil'
-      end
-      it "Pregunta 2" do 
-	expect(@p2.right) == 'falso'
-      end
-      it "Pregunta 3"do
-	expect(@p3.right) == '"HEY!"'
-      end
-      it "Pregunta 4"do 
-	expect(@p4.right) == 'Un objeto'
-      end
-      it "Pregunta 5"do
-	expect(@p5.right) == 'Cierto'
-      end 
-
-      it "Has to have the three components" do
-       expect { Question::SimpleChoice.new(:text=>'5*8=?')}.to raise_error(ArgumentError)
-      end
-    end
-
-    context "Whene converting" do
-      it "can be converted to html" do
-        expect(@sc).to respond_to :to_html
-      end
-
-      it "has to produce a reasonable HTML" do
-	r= %r{<h3>(.*)<\/h3><br\/><input type=\"radio\" value=\"\d\" name=\"0\"\/>\d.*}
-        expect( @sc.to_html ).to match(r)
-      end
-
-    end
-
   end
-end
 end
